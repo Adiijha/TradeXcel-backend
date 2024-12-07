49844e8323e299d1d3b9b6df87d11589729fb7a9
Aditya <adityakumarjha.276@gmail.com>
2024-12-07 11:03:59 +0530

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -5,12 +5,19 @@ import cookieparser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({ origin: 'https://tradexcel.vercel.app' }));
+const corsOptions = {
+  origin: "https://tradexcel.vercel.app", // Allow only your frontend origin
+  credentials: true, // Allow credentials (cookies, authentication headers, etc.)
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended: true, limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieparser())
+app.options("*", cors(corsOptions)); // Enable preflight response for all routes
 
 
 //routes import
